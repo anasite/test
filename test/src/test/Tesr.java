f3971487320ab3a7a9bfe453dd4003e6f60f4e44
Red-Hacker <Red-Hacker@192.168.1.9>
2015-03-08 23:22:09 +0000

    Comit function

--- test/src/test/Tesr.java
+++ test/src/test/Tesr.java
@@ -3,4 +3,11 @@ package test;
 public class Tesr {
 	private int a;
 	private int b;
+	private String myWalo;
+	
+	public void sysoutCl(){
+		OmarClass oclass = new OmarClass();
+		
+		myWalo = oclass.walo();
+	}
 }
